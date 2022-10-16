953fae2171045d536c88c5d971b1f8b3598d83b7
Steven <110969196+Steven-LeMoal@users.noreply.github.com>
2022-10-16 13:04:01 +0200

    Add files via upload

---
+++ Project/veloMax/Statistique.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace veloMax
+{
+    partial class Statistique
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.labelMoyenne = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPiece = new System.Windows.Forms.Button();
+            this.btnModele = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(37, 57);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(510, 239);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(510, 239);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView2);
+            this.panel2.Location = new System.Drawing.Point(573, 57);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(510, 239);
+            this.panel2.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowTemplate.Height = 25;
+            this.dataGridView2.Size = new System.Drawing.Size(510, 239);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dataGridView3);
+            this.panel3.Location = new System.Drawing.Point(37, 324);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(510, 239);
+            this.panel3.TabIndex = 2;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView3.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView3.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.RowTemplate.Height = 25;
+            this.dataGridView3.Size = new System.Drawing.Size(510, 239);
+            this.dataGridView3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Silver;
+            this.panel4.Controls.Add(this.dataGridView4);
+            this.panel4.Location = new System.Drawing.Point(573, 324);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(509, 86);
+            this.panel4.TabIndex = 3;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.AllowUserToAddRows = false;
+            this.dataGridView4.AllowUserToDeleteRows = false;
+            this.dataGridView4.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView4.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView4.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.ReadOnly = true;
+            this.dataGridView4.RowTemplate.Height = 25;
+            this.dataGridView4.Size = new System.Drawing.Size(509, 86);
+            this.dataGridView4.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Silver;
+            this.panel5.Controls.Add(this.labelMoyenne);
+            this.panel5.Controls.Add(this.label1);
+            this.panel5.Location = new System.Drawing.Point(574, 443);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(509, 45);
+            this.panel5.TabIndex = 4;
+            this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel5_Paint);
+            // 
+            // labelMoyenne
+            // 
+            this.labelMoyenne.AutoSize = true;
+            this.labelMoyenne.Location = new System.Drawing.Point(373, 15);
+            this.labelMoyenne.Name = "labelMoyenne";
+            this.labelMoyenne.Size = new System.Drawing.Size(16, 15);
+            this.labelMoyenne.TabIndex = 1;
+            this.labelMoyenne.Text = "...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(295, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "La moyenne des montants des commandes de pièces: ";
+            // 
+            // btnPiece
+            // 
+            this.btnPiece.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnPiece.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPiece.Location = new System.Drawing.Point(37, 23);
+            this.btnPiece.Name = "btnPiece";
+            this.btnPiece.Size = new System.Drawing.Size(128, 28);
+            this.btnPiece.TabIndex = 5;
+            this.btnPiece.Text = "Piece";
+            this.btnPiece.UseVisualStyleBackColor = false;
+            this.btnPiece.Click += new System.EventHandler(this.btnPiece_Click);
+            // 
+            // btnModele
+            // 
+            this.btnModele.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnModele.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModele.Location = new System.Drawing.Point(184, 23);
+            this.btnModele.Name = "btnModele";
+            this.btnModele.Size = new System.Drawing.Size(128, 28);
+            this.btnModele.TabIndex = 6;
+            this.btnModele.Text = "Modele";
+            this.btnModele.UseVisualStyleBackColor = false;
+            this.btnModele.Click += new System.EventHandler(this.btnModele_Click);
+            // 
+            // Statistique
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.ClientSize = new System.Drawing.Size(1117, 644);
+            this.Controls.Add(this.btnModele);
+            this.Controls.Add(this.btnPiece);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Statistique";
+            this.Text = "Statistique";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.Button btnPiece;
+        private System.Windows.Forms.Button btnModele;
+        private System.Windows.Forms.Label labelMoyenne;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Project/veloMax/Statistique.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace veloMax
+{
+    public partial class Statistique : Form
+    {
+        public Statistique()
+        {
+            InitializeComponent();
+            Dgv1();
+            //Dgv1Bis();
+            Dgv2();
+            Dgv3();
+            Dgv4();
+            MoyenneCommande();
+
+        }
+
+        private void Dgv1()
+        {
+            DataConnection connection = new DataConnection();
+            DataTable dt = new DataTable();
+            MySqlDataAdapter command = new MySqlDataAdapter("select Cp.numProduit_CommanderPiece, sum(Cp.quantite) from CommanderPiece as Cp group by Cp.numProduit_CommanderPiece;", connection.Connection);
+            connection.openConnection();
+            command.Fill(dt);
+            dataGridView1.DataSource = dt;
+            connection.closeConnection();
+            string[] columns = { "Num. de la pièce", "Quantité total commandé"};
+            for (int i = 0; i < columns.Length; i++)
+            {
+                dataGridView1.Columns[i].HeaderCell.Value = columns[i];
+            }
+        }
+
+        private void Dgv1Bis()
+        {
+            DataConnection connection = new DataConnection();
+            DataTable dt = new DataTable();
+            MySqlDataAdapter command = new MySqlDataAdapter("select Cm.numProduit_CommanderModele, sum(Cm.quantite) from CommanderModele as Cm group by Cm.numProduit_CommanderModele;", connection.Connection);
+            connection.openConnection();
+            command.Fill(dt);
+            dataGridView1.DataSource = dt;
+            connection.closeConnection();
+            string[] columns = { "Num. du Modèle", "Quantité total commandé" };
+            for (int i = 0; i < columns.Length; i++)
+            {
+                dataGridView1.Columns[i].HeaderCell.Value = columns[i];
+            }
+        }
+
+        private void Dgv2()
+        {
+            DataConnection connection = new DataConnection();
+            DataTable dt = new DataTable();
+            MySqlDataAdapter command = new MySqlDataAdapter("select Pf.descriptionPF, I.nom, I.prenom from Adhere as A join ProgramFidelite as Pf on Pf.numProgramme = A.numProgramme_Adhere join Individu as I on I.idClient = A.idClient_Adhere order by Pf.descriptionPF, I.nom, I.prenom; ", connection.Connection);
+            connection.openConnection();
+            command.Fill(dt);
+            dataGridView2.DataSource = dt;
+            connection.closeConnection();
+            string[] columns = { "Nom du programme", "Nom du client", "Prenom du client" };
+            for (int i = 0; i < columns.Length; i++)
+            {
+                dataGridView2.Columns[i].HeaderCell.Value = columns[i];
+            }
+        }
+
+        private void Dgv3()
+        {
+            DataConnection connection = new DataConnection();
+            DataTable dt = new DataTable();
+            MySqlDataAdapter command = new MySqlDataAdapter("select Pf.descriptionPF, I.nom, I.prenom, DATE_ADD(A.dateAdhesion, interval + Pf.duree year) from Adhere as A join ProgramFidelite as Pf on Pf.numProgramme = A.numProgramme_Adhere join Individu as I on I.idClient = A.idClient_Adhere order by Pf.descriptionPF, I.nom, I.prenom; ", connection.Connection);
+            connection.openConnection();
+            command.Fill(dt);
+            dataGridView3.DataSource = dt;
+            connection.closeConnection();
+            string[] columns = { "Nom du programme", "nom", "prenom","date de fin" };
+            for (int i = 0; i < columns.Length; i++)
+            {
+                dataGridView3.Columns[i].HeaderCell.Value = columns[i];
+            }
+        }
+
+        private void Dgv4()
+        {
+            DataConnection connection = new DataConnection();
+            DataTable dt = new DataTable();
+            MySqlDataAdapter command = new MySqlDataAdapter("select I.nom, I.prenom, sum(Cp.quantite) from Individu as I join CommandeClient as Cc on I.idClient = Cc.idClient_Commande join CommanderPiece as Cp on Cp.numUnique_Commande_P = Cc.numUnique_Commande_C group by Cp.numUnique_Commande_P having sum(Cp.quantite) = max(Cp.quantite); ", connection.Connection);
+            connection.openConnection();
+            command.Fill(dt);
+            dataGridView4.DataSource = dt;
+            connection.closeConnection();
+            string[] columns = { "Nom du Client", "Prenom du Client", "Somme de pièce commandés (max)" };
+            for (int i = 0; i < columns.Length; i++)
+            {
+                dataGridView4.Columns[i].HeaderCell.Value = columns[i];
+            }
+        }
+        
+        private void MoyenneCommande()
+        {
+            double value = 0;
+            int i = 0;
+            DataConnection connection = new DataConnection();
+            connection.openConnection();
+            MySqlCommand command = new MySqlCommand("select avg(Cp.quantite * P.prixUnitaire) from CommanderPiece as Cp join Piece as P on P.numProduit = Cp.numProduit_CommanderPiece group by Cp.numProduit_CommanderPiece; ", connection.Connection);
+            MySqlDataReader data = command.ExecuteReader();
+            while (data.Read())
+            {
+                value += Convert.ToDouble(data["avg(Cp.quantite * P.prixUnitaire)"].ToString());
+                i += 1;
+            }
+            connection.closeConnection();
+            labelMoyenne.Text = Convert.ToString(value / i);
+        }
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnPiece_Click(object sender, EventArgs e)
+        {
+            Dgv1();
+        }
+
+        private void btnModele_Click(object sender, EventArgs e)
+        {
+            Dgv1Bis();
+        }
+    }
+}
